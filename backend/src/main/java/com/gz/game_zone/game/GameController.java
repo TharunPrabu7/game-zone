809712a00ec77e050616acc0e4f868d1e2e7df0e
Tharun Prabu Ra <tharunprabu2418@gmail.com>
2025-06-22 11:07:40 +0530

    added frontend. it now displays the full table

--- backend/src/main/java/com/gz/game_zone/game/GameController.java
+++ backend/src/main/java/com/gz/game_zone/game/GameController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping(path = "api/v1/game")
 public class GameController {
---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import GameDashboard from "./components/GameDashboard";
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1>Game Zone Dashboard</h1>
+      <GameDashboard />
+    </div>
+  );
+};
+
+export default App;
